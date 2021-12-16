c1dbb6143f806be817fe3470f48499712658fe43
deeqamohamed <deeqa-mohamed@outlook.com>
2021-12-16 17:13:10 +0000

    draft4

--- app.py
+++ app.py
@@ -10,9 +10,8 @@ from requests.api import get
 import templates
 from werkzeug.utils import redirect
 from werkzeug.wrappers import response
-import urllib.request
 import csv
-# import error_handling
+import error_handling
 
 
 
@@ -22,6 +21,7 @@ app = Flask(__name__)
 def home():
     if request.method == 'GET':  #GET request is sent first to get web browser
         return render_template("index.html") 
+        
     if request.method=='POST': #POST request to send the input info
         if 'usersfood' and 'userdietary' and 'userhealth' and 'userexclusion' in request.form:
            
@@ -33,27 +33,25 @@ def home():
         return render_template('recipes.html',hits=hits)
     
 def call_api(): # Gets response from api
+
     ingredient=request.form["usersfood"].lower()
     diet=request.form['userdietary'].lower()
     health=request.form['userhealth'].lower()
     exclude=request.form['userexclusion'].lower()
-    recipe_infor=[]
     API_ID = "71b68ced"
     API_KEY = "e23ca4a79da18cff7d516f5e539033e4"
     response = requests.get(f'https://api.edamam.com/api/recipes/v2?type=public&beta=true&q={ingredient}&app_id={API_ID}&app_key={API_KEY}&diet={diet}&health={health}&time=10&imageSize=REGULAR&excluded={exclude}')
     r= response.json()
     hits=r['hits']
-    recipe_infor.append(hits)
-    df = pd.DataFrame.from_dict(recipe_infor)
-    df.to_csv (r'gen.csv', index = False, header=True)
     return hits
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
 
-
-
-        
-
-                
+@app.errorhandler(500)
+def not_found_error(error):
+    return render_template('500.html'), 500            
 
 if __name__== "__main__":
     app.run(debug=True)
