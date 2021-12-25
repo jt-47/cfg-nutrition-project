a85e74500f71a7a2b03831a4ff3ba398f9c54daf
deeqamohamed <deeqa-mohamed@outlook.com>
2021-12-25 23:06:25 +0000

    almost done

--- main.py
+++
@@ -1,6 +0,0 @@
-# from __init_ import made_app
-
-# app=made_app()
-
-# if __name__== "__main__":
-#     app.run(debug=True)  #Remove the Debug=True when the flask app is finished to see the 500 error page
